707625261d10d302da939b785a2a8947a79da34f
Hao-Yu <hh2539@columbia.edu>
2013-11-30 03:34:32 -0500

    full testcases for part A

--- hw6_test.c
+++ hw6_test.c
@@ -5,8 +5,14 @@
 int main(int argc, char **argv) {
 
 	syscall(378, "/data/local/tmp/hw5", "/data/local/tmp/hw8");
-	syscall(378, "/system/bin/sh", "/proc/last_kmsg");
+	syscall(378, "/data/local/tmp/hw5/", "/data/local/tmp/hw8");
+	syscall(378, "/data/local/tmp", "/data/local/tmp/hw8");
+	syscall(378, "/data/local/tmp/hw5", "/data/local/tmp/hw8/");
+	syscall(378, "/data/local/tmp/hw5", "/data/local/tmp/hw8/hwww");
+	syscall(378, "/data/local/tmp/hw5", "/data/local/tmp/");
 	syscall(378, "/data/local/tmp/hw5", "/system/bin/yoyo");
+	syscall(378, "/system/bin/sh", "/proc/last_kmsg");
+	
 
 	return 0;
 }
